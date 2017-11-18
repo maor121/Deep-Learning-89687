555a6338dc408b46ad5c65fed2e68d30692078f0
maor121 <maors1152@gmail.com>
2017-11-18 23:00:25 +0200

    MLP - WIP

--- Assignment1/code/mlpn.py
+++ Assignment1/code/mlpn.py
@@ -3,16 +3,25 @@ import numpy as np
 STUDENT={'name': 'YOUR NAME',
          'ID': 'YOUR ID NUMBER'}
 
-def classifier_output(x, params):
-    # YOUR CODE HERE.
+def classifier_output(x, layers):
+    calculations = do_forward_prop(x, layers)
+    probs = calculations[-1][2]
     return probs
 
 def predict(x, params):
     return np.argmax(classifier_output(x, params))
 
-def loss_and_gradients(x, y, params):
-    # YOU CODE HERE
-    return ...
+def loss_and_gradients(x, y, layers):
+
+    num_labels = len(layers[-1][1])
+
+    T = np.zeros(num_labels)
+    T[y] = 1
+
+    calculations = do_forward_prop(x, layers)
+    grads = do_backprop(layers, calculations, T)
+
+    return grads
 
 def create_classifier(dims):
     """
@@ -34,6 +43,75 @@ def create_classifier(dims):
     to first layer, then the second two are the matrix and vector from first to
     second layer, and so on.
     """
+
     params = []
+
+    dimsIter = iter(dims)
+    lastDim = dimsIter.next()
+    while True:
+        try:
+            nextDim = dimsIter.next()
+            W = np.random.uniform(0,1, (lastDim, nextDim))
+            b = np.random.uniform(0,1, nextDim)
+            params.append((W,b, False))
+
+            lastDim = nextDim
+        except StopIteration:
+            lastLayer = params[-1]
+            params[-1] = (lastLayer[0], lastLayer[1], True)
+            break
     return params
 
+############################# Helper functions ##############################
+
+def softmax(x):
+    shiftx = x - np.max(x)
+    xExp = np.exp(shiftx)
+    x = xExp / np.sum(xExp)
+    return x
+
+def activation(x):
+    return np.tanh(x)
+
+def softmax_deriative(Y, T):
+    return Y - T
+
+def activation_deriative(x):
+    return 1 - (np.tanh(x) ** 2)
+
+def do_forward_prop(X, layers):
+    """Return [(X - input,Z - output before activation,A - output after activation) for each layer"""
+    calculations = []
+    for layer in layers:
+        W, b, is_last_layer = layer
+        Z = calc_layer_output(X, W, b)
+        if is_last_layer:
+            layer_out = softmax(Z)
+        else:
+            layer_out = activation(Z)
+        calculations.append((X, Z, layer_out))
+        X = layer_out
+    return calculations
+
+def do_backprop(layers, forward_prop, T):
+
+    grads = []
+    E = None
+    prev_layer = None
+    for layer, calculation in (reversed(layers), reversed(forward_prop)):
+        is_last = layer[2]
+        if is_last:
+            Y = calculation[2]
+            E = softmax_deriative(Y, T)
+            prev_layer = layer
+        else:
+            Z = calculation[1]
+            W = prev_layer[0]
+            E = activation_deriative(Z) * (E * W.T)
+        X = calculation[0]
+        gW = X.T * E
+        gH = E
+        grads.append([gW, gH])
+
+def calc_layer_output(x, W, b):
+    return np.dot(x, W) + b
