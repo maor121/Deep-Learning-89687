c5c2c462ac7085ed5923b5519af70a740a62206e
maor121 <maors1152@gmail.com>
2017-12-17 14:10:18 +0200

    experiment2 wip

--- Assignment3/code/experiment2.py
+++ Assignment3/code/experiment2.py
@@ -1,13 +1,37 @@
 import random
 from experiment import Generator, LSTMTagger, ModelRunner
 
+def check_positive(str):
+    is_positive = False
+    for i in range(len(str) - 5):
+        all_pass = True
+        for j in range(3):
+            if str[i + j] != str[i + 5 - j]:
+                all_pass = False
+                break
+        if all_pass:
+            is_positive = True
+            break
+    return is_positive
 
 def randomTrainingExample2(C2I, ex_max_len):
     vocab = "0123456789abcd"
-    ex_len = random.randint(len(vocab) * 2 + 1, ex_max_len)
+    ex_len = random.randint(len(vocab), ex_max_len)
 
+    should_positive = random.uniform(0,1) < 0.5
     str = ''.join(random.choice(vocab) for _ in range(ex_len))
-    is_positive = str.count("a") > 2*str.count("b")
+    is_positive = check_positive(str)
+    if should_positive and not is_positive:
+        i = random.randint(0, len(str)-5)
+        sub_str = str[i:i+3]
+        str = str[:i+3] + ''.join(reversed(sub_str)) + str[i+5:]
+        is_positive = True
+    else:
+        while not should_positive and is_positive:
+            str = ''.join(random.choice(vocab) for _ in range(ex_len))
+            is_positive = check_positive(str)
+
+    assert is_positive == check_positive(str)
 
     input_tensor = torch.LongTensor([C2I[c] for c in str])
     category_tensor = torch.LongTensor([is_positive])
@@ -30,9 +54,11 @@ if __name__ == '__main__':
     for c in vocab:
         C2I[c] = len(C2I)
 
+    res = [1 for s,is_pos in [randomTrainingExample2(C2I, 200) for i in range(1000)] if is_pos[0]==1]
+    print("pos/neg: {}/{}".format(len(res),1000-len(res)))
 
     trainloader = Generator(1500, C2I, 200, randomTrainingExample2)
-    testloader = Generator(200, C2I, 10000, randomTrainingExample2)
+    testloader = Generator(200, C2I, 1000, randomTrainingExample2)
 
     runner = ModelRunner(learning_rate, is_cuda)
     runner.initialize_random(embedding_dim, hidden_dim, vocab_size)
