e91a50d35205d036cdb1e673022520e85b91c30c
maor121 <maors1152@gmail.com>
2017-12-18 18:20:23 +0200

    pytorch dynamic batching wip

--- Assignment3/code/bilstmTrain.py
+++ Assignment3/code/bilstmTrain.py
@@ -11,14 +11,17 @@ def sort_by_len(input_tensor, labels_tensor):
 
 class Generator(object):
     def __init__(self, input, labels, batch_size):
-        self._input = input
-        self._labels = labels
+        self._input = np.array(input)
+        self._labels = np.array(labels)
         self._batch_size = batch_size
         self.reset()
+
     def reset(self):
         self.current = 0
+
     def __iter__(self):
         return self
+
     def __next__(self):
         start = self.current * self._batch_size
         end =  (self.current+1) * self._batch_size
@@ -31,6 +34,9 @@ class Generator(object):
         sub_input, sub_labels = self.__shuffle_input_labels(sub_input, sub_labels)
         input_tensor, labels_tensor = self.__build_tensors(sub_input, sub_labels)
         return input_tensor, labels_tensor
+
+    next = __next__  # Python 2 compatibility
+
     @staticmethod
     def __shuffle_input_labels(sub_input, sub_labels):
         num_examples = len(sub_input)
@@ -38,23 +44,93 @@ class Generator(object):
         sub_input = sub_input[permutations]
         sub_labels = sub_labels[permutations]
         return sub_input, sub_labels
+
     @staticmethod
     def __build_tensors(sub_input, sub_labels):
         batch_size = len(sub_input)
         max_seq_len = max([len(seq) for seq in sub_input])
-        seq_depth = sub_input[0].shape(1)
+        seq_depth = sub_input[0].shape[1]
         input_tensor = torch.zeros(batch_size, max_seq_len, seq_depth).long()
-        labels_tensor = torch.LongTensor(sub_labels)
-        for i, e in enumerate(sub_input):
-            l = len(e)
-            offset = max_seq_len - l
+        labels_tensor = torch.zeros(batch_size, max_seq_len)
+        for i, (e,l) in enumerate(zip(sub_input,sub_labels)):
+            length = len(e)
+            offset = max_seq_len - length
             input_tensor[i, offset:max_seq_len] = e
+            labels_tensor[i, offset:max_seq_len] = l
         return input_tensor, labels_tensor
+
+
+class BiLSTMTagger(torch.nn.Module):
+
+    def __init__(self, embedding_dim, hidden_dim, vocab_size):
+        super(BiLSTMTagger, self).__init__()
+        target_size = 2
+
+        self.hidden_dim = hidden_dim
+
+        self.word_embeddings = torch.nn.Embedding(vocab_size, embedding_dim)
+
+        # The LSTM takes word embeddings as inputs, and outputs hidden states
+        # with dimensionality hidden_dim.
+        self.lstm = torch.nn.LSTM(embedding_dim, hidden_dim)
+
+        # The linear layer that maps from hidden state space to tag space
+        self.hidden2tag = torch.nn.Linear(hidden_dim, target_size)
+
+    def init_hidden(self):
+        # Before we've done anything, we dont have any hidden state.
+        # Refer to the Pytorch documentation to see exactly
+        # why they have this dimensionality.
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        self.hidden = (torch.autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
+                       torch.autograd.Variable(torch.zeros(1, 1, self.hidden_dim)))
+
+    def forward(self, sentence):
+        a = sentence
+        words_depth = a.data.shape[1]
+        b = a.view(-1, words_depth)  # Unroll to (batch, 3)
+        c = self.embed1(b)  # To (batch, 5*3, embed_depth)
+        d = c.view(-1, self.window_size * 2 + 1, words_depth, self.embed_depth)  # Roll to (batch, 5, 3, 50)
+        e = d.sum(2)  # Sum along 3rd axis -> (batch, 5, 50)
+        x = e.view(-1, self.embed_depth * (self.window_size * 2 + 1))
+
+        embeds = self.word_embeddings(sentence)
+        lstm_out, self.hidden = self.lstm(
+            embeds.view(len(sentence), 1, -1), self.hidden)
+        out = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        return torch.unsqueeze(out[-1],0)
+
+from experiment import ModelRunner
+class BlistmRunner(ModelRunner):
+    def initialize_random(self, embedding_dim, hidden_dim, vocab_size):
+        net = BiLSTMTagger(embedding_dim, hidden_dim, vocab_size)
+        if (self.is_cuda):
+            # from torch.backends import cudnn
+            # cudnn.benchmark = True
+            net.cuda()
+
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(net.parameters(), lr=self.learning_rate)
+        self.net = net
+
+
 if __name__ == '__main__':
-    W2I, T2I, input_tensor, labels_tensor = utils.load_dataset("../data/train")
+    W2I, T2I, input_list, labels_list = utils.load_dataset("../data/train")
 
-    input_tensor, labels_tensor = sort_by_len(input_tensor, labels_tensor)
+    input_list, labels_list = sort_by_len(input_list, labels_list)
 
+    is_cuda = False
+    batch_size = 128
+    learning_rate = 0.001
+    embedding_dim = 50
+    hidden_dim = T2I.len() * 4
+    vocab_size = W2I.len()
+    epoches = 1
 
+    trainloader = Generator(input_list, labels_list, batch_size)
+
+    runner = ModelRunner(learning_rate, is_cuda)
+    runner.initialize_random(embedding_dim, hidden_dim, vocab_size)
+    runner.train(trainloader, epoches)
 
     print(0)
