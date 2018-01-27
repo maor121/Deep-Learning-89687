148b01a7cc602243821bfd3dba835b05b6ebd6f6
maor121 <maors1152@gmail.com>
2018-01-27 22:30:51 +0200

    report wip

--- Assignment4/code/pre/preprocess.sh
+++ Assignment4/code/pre/preprocess.sh
@@ -1,6 +1,6 @@
 mkdir -p out
 python process-snli.py --data_folder . --out_folder ./out/
 
-python preprocess.py --srcfile ./out/src-train.txt --targetfile ./out/targ-train.txt --labelfile ./out/label-train.txt --srctestfile ./out/src-test.txt --targettestfile ./out/targ-test.txt --labeltestfile ./out/label-test.txt --srcvalfile ./out/src-dev.txt --targetvalfile ./out/targ-dev.txt --labelvalfile ./out/label-dev.txt --outputfile ./out/entail --glove ./data/glove.6B/glove.6B.300d.txt
+python preprocess.py --srcfile ./out/src-train.txt --targetfile ./out/targ-train.txt --labelfile ./out/label-train.txt --srctestfile ./out/src-test.txt --targettestfile ./out/targ-test.txt --labeltestfile ./out/label-test.txt --srcvalfile ./out/src-dev.txt --targetvalfile ./out/targ-dev.txt --labelvalfile ./out/label-dev.txt --outputfile ./out/entail --glove ./glove.6B.300d.txt
 
 python get_pretrain_vecs.py --glove ./glove.6B.300d.txt --outputfile ./out/glove.hdf5 --dictionary ./out/entail.word.dict
